d8eb6fd069341888ce2572f2c14e9e42f504b812
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2020-03-03 10:39:38 -1000

    Completed

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,7 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //Array to display in Table view
+    var favoriteColorsArray = ["Blue","Red", "Green","Orange"]
+    var countriesArray = ["Korea", "Italy", "Spain"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        countriesArray.count
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        let text = countriesArray[indexPath.row]
+        cell?.textLabel?.text = text
+        return cell!
+
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
